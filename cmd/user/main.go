04724bb8c2fc3a966b8610529ae095e09077a432
8thgencore <30128845+8thgencore@users.noreply.github.com>
2024-08-25 21:54:20 +0300

    ✨ Add Config (#2)
    * ✨ Add config

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -2,62 +2,21 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"net"
 
-	pb "github.com/8thgencore/microservice_auth/pkg/user/v1"
-	"github.com/golang/protobuf/ptypes/timestamp"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"github.com/8thgencore/microservice_auth/internal/app"
 )
 
-const grpcPort = 50051
-
-type server struct {
-	pb.UnimplementedUserV1Server
-}
-
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	ctx := context.Background()
+
+	a, err := app.NewApp(ctx)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatal("failed to init app: ", error.Error(err))
 	}
 
-	s := grpc.NewServer()
-	reflection.Register(s)
-	pb.RegisterUserV1Server(s, &server{})
-
-	log.Printf("server listening at %v", lis.Addr())
-
-	if err = s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	err = a.Run()
+	if err != nil {
+		log.Fatal("failed to run app: ", error.Error(err))
 	}
-}
-
-func (s *server) Create(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
-	fmt.Printf("Create user: %+v\n", req)
-	return &pb.CreateUserResponse{Id: 1}, nil
-}
-
-func (s *server) Get(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
-	fmt.Printf("Get user: %d\n", req.GetId())
-	return &pb.GetUserResponse{
-		Id:        req.GetId(),
-		Name:      "John Doe",
-		Email:     "john.doe@example.com",
-		Role:      pb.Role_USER,
-		CreatedAt: &timestamp.Timestamp{Seconds: 1623855600},
-		UpdatedAt: &timestamp.Timestamp{Seconds: 1623855600},
-	}, nil
-}
-
-func (s *server) Update(ctx context.Context, req *pb.UpdateUserRequest) (*pb.Empty, error) {
-	fmt.Printf("Update user: %+v\n", req)
-	return &pb.Empty{}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *pb.DeleteUserRequest) (*pb.Empty, error) {
-	fmt.Printf("Delete user: %d\n", req.GetId())
-	return &pb.Empty{}, nil
 }
