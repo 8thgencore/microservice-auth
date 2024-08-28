a32b85d13b20601762e0d44f2360630dce1e00c6
8thgencore <axelmaker@ya.ru>
2024-08-28 10:57:11 +0300

    ✨ Add service

---
+++ internal/service/user/service.go
@@ -0,0 +1,26 @@
+package user
+
+import (
+	"github.com/8thgencore/microservice_auth/internal/repository"
+	"github.com/8thgencore/microservice_auth/internal/service"
+	"github.com/8thgencore/microservice_auth/pkg/db"
+)
+
+type serv struct {
+	userRepository repository.UserRepository
+	logRepository  repository.LogRepository
+	txManager      db.TxManager
+}
+
+// NewService creates new object of service layer.
+func NewService(
+	userRepository repository.UserRepository,
+	logRepository repository.LogRepository,
+	txManager db.TxManager,
+) service.UserService {
+	return &serv{
+		userRepository: userRepository,
+		logRepository:  logRepository,
+		txManager:      txManager,
+	}
+}
