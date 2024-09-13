db2ad44c0402da9c15bd454c23cf53380dcb6c75
8thgencore <30128845+8thgencore@users.noreply.github.com>
2024-09-13 10:25:53 +0300

    🛠️ Update tests (#8)
    * ✨ Add gitguardian
    
    * ✨ Add password for redis
    
    * 🛠️  Fix test
    
    * 🛠️ Fix tests
    
    * 🛠️ Fix user tests
    
    * 🛠️ Fix test

--- internal/service/user/user.go
+++ internal/service/user/user.go
@@ -54,7 +54,6 @@ func (s *serv) Create(ctx context.Context, user *model.UserCreate) (int64, error
 		if errors.Is(err, ErrUserEmailExists) {
 			return 0, ErrUserEmailExists
 		}
-
 		return 0, ErrUserCreate
 	}
 
